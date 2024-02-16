85da563ecb08c09d5384ab164175b7a67497bd7b
buravelliprasad <138657173+buravelliprasad@users.noreply.github.com>
2024-02-16 18:43:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -655,14 +655,18 @@ with container:
             with col1:
                 st.image("icon-1024.png", width=50)
             with col2:
-                st.markdown(
-                f'<div style="background-color: black; color: white; border-radius: 10px; padding: 10px; width: 60%;'
-                f' border-top-right-radius: 10px; border-bottom-right-radius: 10px;'
-                f' border-top-left-radius: 0; border-bottom-left-radius: 0; box-shadow: 2px 2px 5px #888888;">'
-                f'<span style="font-family: Arial, sans-serif; font-size: 16px; white-space: pre-wrap;">{answer}</span>'
-                f'</div>',
-                unsafe_allow_html=True
-            )
+            #     st.markdown(
+            #     f'<div style="background-color: black; color: white; border-radius: 10px; padding: 10px; width: 60%;'
+            #     f' border-top-right-radius: 10px; border-bottom-right-radius: 10px;'
+            #     f' border-top-left-radius: 0; border-bottom-left-radius: 0; box-shadow: 2px 2px 5px #888888;">'
+            #     f'<span style="font-family: Arial, sans-serif; font-size: 16px; white-space: pre-wrap;">{answer}</span>'
+            #     f'</div>',
+            #     unsafe_allow_html=True
+            # )
+                st.markdown(answer)
+               
+            
+
 
         if st.session_state.user_name:
             try:
